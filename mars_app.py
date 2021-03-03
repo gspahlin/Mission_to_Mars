e2c23f3d236e14469b0acacffb01eedd06289b09
gspahlin <gspahlin@gmail.com>
2021-03-03 10:48:50 -0500

    working version of the flask app

--- mars_app.py
+++ mars_app.py
@@ -13,19 +13,22 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+
+
 @app.route('/')
 def index():
-    mars_data = mars_data.mongo.db.find_one()
-    return render_template('index.html', title = mars_data['news_headline'],  para = mars_data['news_blurb'],
-    m_url = mars_data['daily_img'], m_html = mars_data['html_d_table'], cerb_t = mars_data['hemi_pics'][0]['title'], 
-    cerb_url = mars_data['hemi_pics'][0]['url'], schi_t = mars_data['hemi_pics'][1]['title'], 
-    schi_url = mars_data['hemi_pics'][1]['url'], syr_t = mars_data['hemi_pics'][2]['title'], syr_url = mars_data['hemi_pics'][2]['url'],
-    val_t = mars_data['hemi_pics'][3]['title'],  val_url = mars_data['hemi_pics'][3]['url'])
+    data = mongo.db.mars_data.find_one()
+    return render_template('index.html', title = data['news_headline'],  para = data['news_blurb'],
+    m_url = data['daily_img'], m_html = data['html_d_table'], cerb_t = data['hemi_pics'][0]['title'], 
+    cerb_url = data['hemi_pics'][0]['url'], schi_t = data['hemi_pics'][1]['title'], 
+    schi_url = data['hemi_pics'][1]['url'], syr_t = data['hemi_pics'][2]['title'], syr_url = data['hemi_pics'][2]['url'],
+    val_t = data['hemi_pics'][3]['title'],  val_url = data['hemi_pics'][3]['url'])
 
 @app.route('/scrape')
 def scrapie():
-    mars_data  = scrape_mars.scrape_mars()
-    mars_data.update({}, mars_data, upsert=True)
+    mars_data = mongo.db.mars_data
+    m_data  = scrape_mars.scrape_mars()
+    mars_data.update({}, m_data, upsert=True)
     return redirect("/", code=302)
 
 
